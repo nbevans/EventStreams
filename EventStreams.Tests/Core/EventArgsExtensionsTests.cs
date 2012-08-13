f18003ad6be88e3e784abc4361aebb80f1b5ba86
Nathan Evans <nathan.evans@parkerfox.co.uk>
2012-08-13 11:49:47 +0100

    Another small set of tests added.

---
+++ EventStreams.Tests/Core/EventArgsExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EventStreams.Domain.Events.BankAccount;
+using NUnit.Framework;
+
+namespace EventStreams.Core {
+
+    [TestFixture]
+    public class EventArgsExtensionsTests {
+
+        private EventArgs _event;
+
+        [TestFixtureSetUp]
+        public void Given() {
+            _event = new Credited(0);
+        }
+
+        [Test]
+        public void When_assumed_correctly_to_be_a_credited_event_then_it_is_casted_fine_and_is_not_null() {
+            Assert.That(_event.Assume<Credited>(), Is.Not.Null);
+        }
+
+        [Test]
+        public void When_assumed_incorrectly_to_be_a_debited_event_then_it_will_throw() {
+            Assert.Throws<InvalidOperationException>(() => _event.Assume<Debited>());
+        }
+    }
+}
