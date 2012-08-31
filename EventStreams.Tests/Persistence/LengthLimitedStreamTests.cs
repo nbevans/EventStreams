a8d87eed7c0b5eeb4c4c0773e61e0271f962da91
Nathan Evans <nathan.evans@parkerfox.co.uk>
2012-08-31 15:23:17 +0100

    Implemented a stream decorator that will come in handy to prevent StreamReader from buffering too far ahead in a stream. This will be essential when implementing the Json Body reading.

---
+++ EventStreams.Tests/Persistence/LengthLimitedStreamTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using EventStreams.Persistence.Serialization;
+using NUnit.Framework;
+
+namespace EventStreams.Persistence {
+
+    [TestFixture]
+    public class LengthLimitedStreamTests {
+
+        private readonly byte[] _data = Encoding.UTF8.GetBytes("{ }_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text_trailing-text");
+        private readonly string _expected = "{ }";
+
+        [Test]
+        public void Given_some_data_when_length_limited_to_3_bytes_and_read_to_end_with_default_buffering_then_read_data_is_as_expected_and_position_has_not_exceeeded_limit() {
+            using (var ms = new MemoryStream()) {
+                ms.Write(_data, 0, _data.Length);
+                ms.Position = 0;
+
+                using (var lls = new LengthLimitedStream(ms, 3))
+                using (var sr = new StreamReader(lls)) {
+                    var result = sr.ReadToEnd();
+                    Assert.AreEqual(_expected, result);
+                    Assert.That(ms.Position, Is.EqualTo(3));
+                }
+
+                ms.Position = 0;
+
+                using (var lls = new LengthLimitedStream(ms, 3)) {
+                    Assert.That(lls.ReadByte() == '{');
+                    Assert.That(lls.ReadByte() == ' ');
+                    Assert.That(lls.ReadByte() == '}');
+                    Assert.That(ms.Position, Is.EqualTo(3));
+                }
+            }
+        }
+
+        [Test]
+        public void Given_some_data_when_length_limited_to_3_bytes_and_read_first_3_bytes_individually_with_minimal_buffering_then_read_data_is_as_expected_and_position_has_not_exceeeded_limit() {
+            using (var ms = new MemoryStream()) {
+                ms.Write(_data, 0, _data.Length);
+                ms.Position = 0;
+
+                using (var lls = new LengthLimitedStream(ms, 3))
+                using (var sr = new StreamReader(lls, Encoding.UTF8, false, 1)) {
+                    Assert.That(sr.Read() == _expected[0]);
+                    Assert.That(sr.Read() == _expected[1]);
+                    Assert.That(sr.Read() == _expected[2]);
+                    Assert.That(ms.Position, Is.EqualTo(3));
+                }
+            }
+        }
+
+        [Test]
+        public void Given_some_data_when_length_limited_to_3_bytes_and_read_to_end_with_zero_buffering_then_read_data_is_as_expected_and_position_has_not_exceeeded_limit() {
+            using (var ms = new MemoryStream()) {
+                ms.Write(_data, 0, _data.Length);
+                ms.Position = 0;
+
+                using (var lls = new LengthLimitedStream(ms, 3)) {
+                    Assert.That(lls.ReadByte() == _expected[0]);
+                    Assert.That(lls.ReadByte() == _expected[1]);
+                    Assert.That(lls.ReadByte() == _expected[2]);
+                    Assert.That(ms.Position, Is.EqualTo(3));
+                }
+            }
+        }
+
+        [Test]
+        public void Given_a_length_limited_stream_when_doing_something_other_than_reading_then_expect_it_to_throw() {
+            using (var ms = new MemoryStream())
+            using (var lls = new LengthLimitedStream(ms, 3)) {
+                Assert.Throws<NotSupportedException>(() => lls.Write(new byte[5], 0, 5));
+                Assert.Throws<NotSupportedException>(() => lls.Seek(5, SeekOrigin.Current));
+                Assert.Throws<NotSupportedException>(() => lls.SetLength(5));
+                Assert.Throws<NotSupportedException>(lls.Flush);
+            }
+        }
+    }
+}
---
+++ EventStreams/Persistence/Serialization/LengthLimitedStream.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace EventStreams.Persistence.Serialization {
+    public class LengthLimitedStream : Stream {
+        private readonly Stream _innerStream;
+        private readonly long _lengthLimit;
+
+        public LengthLimitedStream(Stream innerStream, long count) {
+            if (innerStream == null) throw new ArgumentNullException("innerStream");
+            _innerStream = innerStream;
+            _lengthLimit = innerStream.Position + count;
+        }
+
+        public override void Flush() {
+            throw new NotSupportedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin) {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value) {
+            throw new NotSupportedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count) {
+            if (_lengthLimit - Position > 0) {
+                var a = Math.Min(_lengthLimit, Position + count);
+                var b = Math.Min(a, count);
+
+                return _innerStream.Read(buffer, offset, (int)b);
+            }
+
+            return 0;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count) {
+            throw new NotSupportedException();
+        }
+
+        public override bool CanRead {
+            get { return _innerStream.CanRead; }
+        }
+
+        public override bool CanSeek {
+            get { return _innerStream.CanSeek; }
+        }
+
+        public override bool CanWrite {
+            get { return false; }
+        }
+
+        public override long Length {
+            get { return _lengthLimit; }
+        }
+
+        public override long Position {
+            get { return _innerStream.Position; }
+            set { _innerStream.Position = value; }
+        }
+    }
+}
