31c20369e8a7bbeadb4132ff4820359b61734705
Nathan Evans <nathan.evans@pebblecode.com>
2012-11-16 16:20:05 +0000

    Removed some orphan types.

--- EventStreams.Core/Core/Domain/IAggregateRootRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace EventStreams.Core.Domain {
-    public interface IAggregateRootRepository<TAggregateRoot> where TAggregateRoot : class, IObserver<EventArgs>, new() {
-        TAggregateRoot Create();
-        TAggregateRoot Get(Guid identifier);
-        bool TryGet(Guid identifier, out TAggregateRoot aggregateRoot);
-    }
-}
